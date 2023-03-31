23293d124517fd144ad2ae371c19ca46e1e91d84
Anh Tester <thaian.it15@gmail.com>
2023-03-31 12:12:49 +0700

    Update lesson 33 - Extent Report

---
+++ src/main/java/anhtester/com/reports/ExtentReportManager.java
@@ -0,0 +1,19 @@
+package anhtester.com.reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager {
+
+    private static final ExtentReports extentReports = new ExtentReports();
+
+    public synchronized static ExtentReports getExtentReports() {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("reports/extentreport/extentreport.html");
+        reporter.config().setReportName("Extent Report | Anh Tester");
+        extentReports.attachReporter(reporter);
+        extentReports.setSystemInfo("Framework Name", "Selenium Java | Anh Tester");
+        extentReports.setSystemInfo("Author", "Anh Tester");
+        return extentReports;
+    }
+
+}
---
+++ src/main/java/anhtester/com/reports/ExtentTestManager.java
@@ -0,0 +1,51 @@
+package anhtester.com.reports;
+
+import anhtester.com.drivers.DriverManager;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtentTestManager {
+    static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
+    static ExtentReports extent = ExtentReportManager.getExtentReports();
+
+    public static ExtentTest getTest() {
+        return extentTestMap.get((int) Thread.currentThread().getId());
+    }
+
+    public static synchronized ExtentTest saveToReport(String testName, String desc) {
+        ExtentTest test = extent.createTest(testName, desc);
+        extentTestMap.put((int) Thread.currentThread().getId(), test);
+        return test;
+    }
+
+    public static void addScreenShot(String message) {
+        String base64Image = "data:image/png;base64,"
+                + ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BASE64);
+
+        getTest().log(Status.INFO, message,
+                MediaEntityBuilder.createScreenCaptureFromBase64String(base64Image).build());
+    }
+
+    public static void addScreenShot(Status status, String message) {
+        String base64Image = "data:image/png;base64,"
+                + ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BASE64);
+
+        getTest().log(status, message,
+                MediaEntityBuilder.createScreenCaptureFromBase64String(base64Image).build());
+    }
+
+    public static void logMessage(String message) {
+        getTest().log(Status.INFO, message);
+    }
+
+    public static void logMessage(Status status, String message) {
+        getTest().log(status, message);
+    }
+}
